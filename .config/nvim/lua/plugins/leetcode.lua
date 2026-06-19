ef02b68e1a0bca84a90dc3882a78c0346bd546c3
DFranklin93 <dfranklin2203@gmail.com>
2026-06-19 12:48:20 -0400

    Adding leetcode plugin

---
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lang = "rust", -- or cpp/java/rust/go/etc
+    }
+  }
+}
+
