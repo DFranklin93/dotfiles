a748f337cf856d1561464867cc7401e5d55c8715
DFranklin93 <don2226@live.com>
2026-06-19 12:17:00 -0400

    Adding relativenumber, cursorline, and scrolloff to vim options

--- .config/nvim/lua/config/vim-options.lua
+++ .config/nvim/lua/config/vim-options.lua
@@ -1,5 +1,9 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+vim.cmd("set number") -- line numbers
+vim.cmd("set relativenumber") -- relative line numbers
+vim.cmd("set cursorline") -- highlight current line
+vim.cmd("set scrolloff=10") -- keep 10 lines above/below cursor
+
+vim.cmd("set expandtab") -- use spaces instead of tabs
+vim.cmd("set tabstop=2") -- tab width
+vim.cmd("set softtabstop=2") -- soft tab stop not tabs on tab/backspace
+vim.cmd("set shiftwidth=2") -- indent width
